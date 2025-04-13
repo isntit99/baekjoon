a4832384ff8bc4a5937c4ac5ee22b955c177e8b0
isntit99 <aladdinjinny@gmail.com>
2025-04-13 17:10:00 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+   
+    return 0;
+}
